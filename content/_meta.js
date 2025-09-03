d7fce1f1d2c353ce5a1cdc02b1ca03ec797be939
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-09-03 20:28:39 +0200

    init

---
+++ content/_meta.js
@@ -0,0 +1,16 @@
+export default {
+  'index': 'MS Annika',
+  'download': 'Download',
+  'install': 'Installation',
+  'usage': 'Usage',
+  'tutorial': 'Tutorial',
+  'example': 'Example Data',
+  'workflows': 'Example Workflows',
+  'astral': 'Astral Data',
+  'tims': 'MGF and timsTOF Data',
+  'help': 'Getting Help',
+  'issues': 'Known Issues',
+  'contrib': 'Contributing',
+  'citing': 'Citing',
+  'contact': 'Contact'
+}
